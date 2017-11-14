3848dac0d007ea94da0135a55181ccc3ad17e572
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2017-11-14 13:44:03 +0900

    hello, world

---
+++ C++Tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Tutorial
+//
+//  Created by tomatoaiu on 2017/11/14.
+//  Copyright © 2017年 tomatoaiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
